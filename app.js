4c13d6fdcaab67d1f4887f5a2cdb4b911b547257
Edward Fleming <ed@kinvey.com>
2015-07-08 13:06:42 -0400

    changing port

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/authenticate', function (req, res) {
 
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
 
   var host = server.address().address;
   var port = server.address().port;
